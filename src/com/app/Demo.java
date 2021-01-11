a67719c6c92e76957ae32d4f8ff7953a64e601b2
Sumit Rahate <71226353+sumit2618@users.noreply.github.com>
2021-01-11 10:43:06 +0530

    Update Demo.java

--- src/com/app/Demo.java
+++ src/com/app/Demo.java
@@ -2,6 +2,6 @@ package com.app;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("Code added by Sumit");
+		System.out.println("Code added by Sumit 11/1/2021");
 	}
 }
